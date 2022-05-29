c644dd292ff083f4dfe0721c66253775a086680b
Ahmed Hanzala <ahanzala.bscs20seecs@seecs.edu.pk>
2022-05-29 14:40:13 +0500

    Fixing Buggy face detection

--- app_httpd.cpp
+++ app_httpd.cpp
@@ -1,15 +1,4 @@
-// Copyright 2015-2016 Espressif Systems (Shanghai) PTE LTD
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+
 // limitations under the License.
 #include "esp_http_server.h"
 #include "esp_timer.h"
@@ -642,7 +631,7 @@ void startCameraServer(){
     mtmn_config.o_threshold.score = 0.7;
     mtmn_config.o_threshold.nms = 0.7;
     mtmn_config.o_threshold.candidate_number = 1;
-    
+    mtmn_config = mtmn_init_config();
     face_id_init(&id_list, FACE_ID_SAVE_NUMBER, ENROLL_CONFIRM_TIMES);
     
     Serial.printf("Starting web server on port: '%d'\n", config.server_port);
